9e86b63183f489b61ba4dce25663d0e4d3014d3c
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2020-03-30 15:25:32 +0200

    reshape synei_a to connectivity matrix. tested. closes #13

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import utils
 
 
-class Test_convert_to_adjacency_matrix(unittest.TestCase):
+class Test_convert_to_EE_adjacency_matrix(unittest.TestCase):
 
 
     def test_constructed_adj_matches_expected_output(self):
@@ -16,7 +16,7 @@ class Test_convert_to_adjacency_matrix(unittest.TestCase):
         syn_array = synee_a['a'][-1]
         i, j = synee_a['i'], synee_a['j']
         
-        adj_m = utils.convert_to_adjacency_matrix(syn_array, 400)
+        adj_m = utils.convert_to_EE_adjacency_matrix(syn_array, 400)
 
         for k in np.random.choice(range(400*399),1000):
             self.assertEqual(adj_m[i[k],j[k]], syn_array[k]) 
@@ -30,12 +30,31 @@ class Test_convert_to_adjacency_matrix(unittest.TestCase):
         syn_array = synee_a['a'][-1]
         i, j = synee_a['i'], synee_a['j']
         
-        adj_m = utils.convert_to_adjacency_matrix(syn_array, 400)
+        adj_m = utils.convert_to_EE_adjacency_matrix(syn_array, 400)
 
         for k in range(400):
             self.assertEqual(adj_m[k,k],0) 
 
 
+
+
+class Test_convert_to_EI_adjacency_matrix(unittest.TestCase):
+
+
+    def test_constructed_adj_matches_expected_output(self):
+
+        with open('test/test_sets/synei_a_with-ij.p', 'rb') as pfile:
+            synee_a = pickle.load(pfile)
+
+        syn_array = synee_a['a'][-1]
+        i, j = synee_a['i'], synee_a['j']
+        
+        adj_m = utils.convert_to_EI_adjacency_matrix(syn_array, 400, 80)
+
+        for k in np.random.choice(range(400*80),1000):
+            self.assertEqual(adj_m[i[k],j[k]], syn_array[k]) 
+
+            
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-def convert_to_adjacency_matrix(syn_array, N):
+def convert_to_EE_adjacency_matrix(syn_array, N):
 
     assert np.shape(syn_array) == tuple([N*(N-1)]), \
         "Synpase array doesn't have the expected shape"
@@ -19,76 +19,25 @@ def convert_to_adjacency_matrix(syn_array, N):
         adj_matrix[row_id, col_id] = syn_el
 
     return adj_matrix
+
+
+def convert_to_EI_adjacency_matrix(syn_array, N_E, N_I):
+
+    assert np.shape(syn_array) == tuple([N_I*N_E]), \
+        "Synpase array doesn't have the expected shape"
+
+    # construct i,j arrays
+    i = np.repeat(range(N_I), N_E)
+    j = np.tile(range(N_E), N_I)
+
+    adj_matrix = np.zeros((N_I,N_E))
+
+    for syn_el, row_id, col_id in zip(syn_array,i,j):
+        adj_matrix[row_id, col_id] = syn_el
+
+    return adj_matrix
+
         
-    
-
-# import pickle
-# with open('synee_e.p', 'rb') as pfile:
-#     sa= picke.load(pfile)
-# with open('synee_a.p', 'rb') as pfile:
-#     sa= picke.load(pfile)
-# with open('synee_a.p', 'rb') as pfile:
-#     sa= picklee.load(pfile)
-# with open('synee_a.p', 'rb') as pfile:
-#     sa= pickle.load(pfile)
-# sa
-# sa.keys()
-# sa['i']
-# np.shape(sa['i'])
-# sa.keys()
-# np.shape(sa['syn_active'])
-# np.shape(sa['syn_active'][-1])
-# A = sa['syn_active'][-1]
-# np.reshape(A, (400,400))
-# 400*399
-# np.reshape(A, (400,399))
-# np.reshape(sa['i'], (400,399))
-# np.reshape(sa['j'], (400,399))
-# np.reshape(sa['j'], (399,400))
-# np.reshape(sa['j'], (400,399))
-# for i,j in zip(sa['i'], sa['j']):
-#     print(i,j)
-# sx = np.zeros((400,400))
-# for i,j in zip(sa['i'], sa['j']):
-#     sx[i,j] = i*10**3+j
-# xs
-# sx
-# print(sx)
-# np.repeat([0,1,2], 3)
-# np.tile([0,1,2], 3)
-# x = np.tile([0,1,2], 3)
-# x =  np.repeat([0,1,2], 3)
-# y =  np.tile([0,1,2], 3)
-# x
-# y
-# idx = (x == y)
-# idx
-# np.invert(idx)
-# x[np.invert(idx)]
-# y[np.invert(idx)]
-# xl = np.repeat(range(400), 400)
-# yl = np.tile(range(400), 400)
-# idxl = (xl==yl)
-# xx = xl[idxl]
-# xx
-# cl
-# xl
-# yl
-# len(xl)
-# yl
-# len(yl)
-# idx
-# idxl
-# xx = xl[np.invert(idxl)]
-# yy = yl[np.invert(idxl)]
-# xx
-# yy
-# np.testing.assert_array_equal(xx, sa['i'])
-# np.testing.assert_array_equal(yy, sa['i'])
-# np.testing.assert_array_equal(yy, sa['j'])
-# %hist
-
-
 
 def topright_axis_off(ax):
 
