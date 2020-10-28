274babc0c41d9def1eb8f86969b19055f5d5700a
Jan Marker <jan@jangmarker.de>
2020-10-28 23:33:39 +0100

    Add branching_ratio to post_processing

--- branching_ratio.py
+++ branching_ratio.py
@@ -59,7 +59,15 @@ def h_lin(rk: mre.CoefficientResult, exp_fit: mre.FitResult) -> bool:
     return lin_fit.ssres < exp_fit.ssres
 
 
-def branching_ratio_figure(bpath, nsp):
+def branching_ratio_figure(bpath):
+    nsp = None
+    try:
+        with open(bpath+'/raw/namespace.p', 'rb') as pfile:
+            nsp=pickle.load(pfile)
+    except FileNotFoundError:
+        print(bpath[-4:], "reports: No namespace data. Skipping.")
+        return
+
     size_factor = 2
     rc('font', size=str(8*size_factor))
 
@@ -131,28 +139,13 @@ def branching_ratio_figure(bpath, nsp):
     pl.savefig(directory+"/{:s}.png".format(bpath[-4:]),
                dpi=300, bbox_inches='tight')
 
-    
-
-
 
     
 if __name__ == "__main__":
-
-    
     # return a list of each build (simulations run)
     # e.g. build_dirs = ['builds/0003', 'builds/0007', ...]
     # sorted to ensure expected order
     build_dirs = sorted(['builds/'+pth for pth in next(os.walk("builds/"))[1]])
     
     for bpath in build_dirs:
-
-        try:
-            with open(bpath+'/raw/namespace.p', 'rb') as pfile:
-                nsp=pickle.load(pfile)
-                
-            # if bpath[-4:]=='0000':
-
-            branching_ratio_figure(bpath, nsp)
-
-        except FileNotFoundError:
-            print(bpath[-4:], "reports: No namespace data. Skipping.")
+        branching_ratio_figure(bpath)
